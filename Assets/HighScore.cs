d034d7ff992f5bde4d6ea84d5bc7420de9ecefbf
Lin Xu <lin.xu@uni-ulm.de>
2016-04-27 12:05:19 +0200

    snake ar

---
+++ Assets/HighScore.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script just stores the high score data for GameManager
+// to use. It will not get destroyed when a new scene is loaded
+// (or in our case, when the same scene is reloaded).
+public class HighScore : MonoBehaviour {
+	public int currentHighScore;
+
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+	}	
+}
---
+++ Assets/SnakeBodyTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeBodyTrigger : MonoBehaviour {
+	// OnTriggerEnter is our newest Unity event function.
+	// Unity calls it if the script's GameObject has a trigger collider,
+	// and some other object's collider enters the trigger collider volume.
+	//
+	// (Trigger collider means any collider component, such as
+	// Sphere Collider, Box Collider etc. that has the Is Trigger
+	// option enabled.)
+	//
+	// A moving trigger object should have a Rigidbody component as well,
+	// otherwise the physics system does not deal well with it.
+	// In this exercise, we use a Kinematic Rigidbody (Rigidbody with
+	// the Is Kinematic option) because we don't want any actual physical
+	// behavior like falling or bumping, just for the trigger to work.
+	void OnTriggerEnter(Collider c) {
+		GameObject go = GameObject.Find("GameManager");
+		GameManager gm = go.GetComponent<GameManager>();
+		gm.TriggerGameOver();
+	}
+}
